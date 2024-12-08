7251eab28c29109ac6b7fb1e548bd3de5cfbe567
Geetansh Dua <geetanshdua11@gmail.com>
2024-12-08 20:28:33 +0530

    Game Environment

--- main.cpp
+++ main.cpp
@@ -9,6 +9,20 @@ const int WINDOW_HEIGHT = 600;
 SDL_Window* window = nullptr;
 SDL_Renderer* renderer = nullptr;
 
+struct Paddle {
+	int x, y, w,  h;
+	int velY;//Velocity Y
+};
+
+struct Ball {
+	int x, y, w, h;
+	int velX, velY;
+};
+
+Paddle leftPaddle = {50, WINDOW_HEIGHT / 2 - 50, 15, 100, 0};
+Paddle rightPaddle = { WINDOW_WIDTH - 65, WINDOW_HEIGHT / 2 - 50, 15, 100, 0 };
+Ball ball = { WINDOW_WIDTH / 2 - 10, WINDOW_HEIGHT / 2 - 10, 20, 20, 5, 5 };
+
 bool init() {
 	if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
 		std::cerr << "SDL2 could not initialized! SDL_Error: " << SDL_GetError() << std::endl;
@@ -42,7 +56,6 @@ bool init() {
 		return false;
 	}
 
-	SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
 	return true;
 
 }
@@ -71,11 +84,26 @@ int main(int argc, char* argv[]) {
 			}
 		}
 
+		//ball.x += ball.velX;
+		//ball.y += ball.velY;
+
+
+		SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
 		SDL_RenderClear(renderer);
 
+		SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+		
+		SDL_Rect leftRect = {leftPaddle.x, leftPaddle.y, leftPaddle.w, leftPaddle.h};
+		SDL_Rect rightRect = { rightPaddle.x, rightPaddle.y, rightPaddle.w, rightPaddle.h };
+		SDL_Rect ballRect = {ball.x, ball.y, ball.w, ball.h};
 
+		SDL_RenderFillRect(renderer, &leftRect);
+		SDL_RenderFillRect(renderer, &rightRect);
+		SDL_RenderFillRect(renderer, &ballRect);
 
 		SDL_RenderPresent(renderer);
+
+		SDL_Delay(16);
 	}
 
 	close();
